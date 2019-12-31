f99aaafbab0fa94a911b8e2346017775b68a11ea
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-12-31 15:37:46 -0600

    continue working

--- explore.py
+++ explore.py
@@ -9,3 +9,12 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import prepare
+
+def plot_columns(df):
+    for col in df.columns:
+        plt.figure(figsize = (16,4))
+        plt.plot(df.resample('W').sum()[col])
+        plt.xlabel('date')
+        plt.ylabel(col)
+        plt.show()
+        
--- prepare.py
+++ prepare.py
@@ -4,3 +4,14 @@ from os import path
 
 import warnings
 warnings.filterwarnings('ignore')
+
+
+def prep_data(df):
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.sort_values(by='date').set_index('date')
+    df['calories_burned'] = df['calories_burned'].str.replace(',','')
+    df['steps'] = df['steps'].str.replace(',','')
+    df['minutes_sedentary'] = df['minutes_sedentary'].str.replace(',','')
+    df['activity_calories'] = df['activity_calories'].str.replace(',','')
+    df[['calories_burned', 'steps', 'minutes_sedentary', 'activity_calories']] = df[['calories_burned', 'steps', 'minutes_sedentary', 'activity_calories']].apply(pd.to_numeric)
+    return df
